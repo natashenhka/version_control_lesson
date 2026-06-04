adbf6ec4eda247599b710feacf2e10bc4bf0e317
natashenhka <muzitskaya@gmail.com>
2026-06-04 20:08:45 +0300

    print deprecated

--- analytics.py
+++ analytics.py
@@ -6,6 +6,6 @@ import seaborn as sns
 #create data frame from dictionary
 df = pd.DataFrame({'Name': ['Olga', 'Maks', 'Slava'], 'Age': [22, 34, 45]})
 #print df head
-print(df.head())
+#print(df.head())
 sns.barplot(data=df, x='Name', y='Age')
 plt.show()
