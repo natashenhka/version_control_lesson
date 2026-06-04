bb56488b18b0d1f6d37c13976d8a4be5d7816fde
natashenhka <muzitskaya@gmail.com>
2026-06-04 19:54:34 +0300

    analysis appended with barplot for name_age

--- analytics.py
+++ analytics.py
@@ -1,7 +1,11 @@
 #import libraries
 import pandas as pd
 import numpy as np
-#create data freame from dictionary
+import matplotlib.pyplot as plt
+import seaborn as sns
+#create data frame from dictionary
 df = pd.DataFrame({'Name': ['Olga', 'Maks', 'Slava'], 'Age': [22, 34, 45]})
 #print df head
 print(df.head())
+sns.barplot(data=df, x='Name', y='Age')
+plt.show()
